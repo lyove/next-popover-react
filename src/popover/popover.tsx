f00da668161718aab9790dc532de2f85dabfc31a
Lyove <lyove@outlook.com>
2023-12-06 15:29:27 +0800

    init

--- src/popover/popover.tsx
+++ src/popover/popover.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, Children, ReactElement, cloneElement } from "react";
+import { useEffect, useRef, Children, ReactElement, cloneElement } from "react";
 import NextPopover from "next-popover";
 import type { PopoverProps } from "./type";
 
@@ -9,7 +9,6 @@ import type { PopoverProps } from "./type";
  */
 function Popver(props: PopoverProps) {
   const popRef = useRef<any>(null);
-  const [popInst, setPopInst] = useState<any>(null);
 
   // Child Node
   const child = Children.only(props.children) as ReactElement;
@@ -18,16 +17,14 @@ function Popver(props: PopoverProps) {
   });
 
   useEffect(() => {
-    const { content, ...restProps } = props;
-    let popover = null;
+    const { content, ...restProps } = props;;
     if (popRef.current) {
-      popover = new NextPopover({
+      new NextPopover({
         ...restProps,
         trigger: popRef.current,
         content,
       });
     }
-    setPopInst(popover);
   }, []);
 
   return triggerNode;
